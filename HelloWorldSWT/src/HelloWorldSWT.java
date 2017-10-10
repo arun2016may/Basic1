e37f8d864ee7b69f9b71d0af1a1660a4009f057f
arun2016may <arunk@us.ibm.com>
2017-10-10 10:49:33 -0400

    first one

---
+++ HelloWorldSWT/src/HelloWorldSWT.java
@@ -0,0 +1,18 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class HelloWorldSWT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Display display = new Display();
+        Shell shell = new Shell(display);
+        shell.setText("Loading files - Please wait...");
+        shell.open();
+        while (!shell.isDisposed()) {
+           if (!display.readAndDispatch()) display.sleep();
+}
+      display.dispose();
+	}
+
+}
